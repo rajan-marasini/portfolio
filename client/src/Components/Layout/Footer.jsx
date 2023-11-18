d8654291e2c9627a60e1e648df31db2f33aba296
Rajan Marasini <122443030+rajan-marasini@users.noreply.github.com>
2023-11-18 11:53:05 +0545

    adminlogin

--- client/src/Components/Layout/Footer.jsx
+++ client/src/Components/Layout/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
     return (
         <div className="text-center sticky bottom-0 p-4">
-            <p>All right reserve &copy; Rajan Marasini</p>
+            <p>
+                All{" "}
+                <Link className="cursor-default" to={"/adminlogin"}>
+                    right
+                </Link>{" "}
+                reserve &copy; Rajan Marasini
+            </p>
         </div>
     );
 };
